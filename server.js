b2ec89221d2c761868842f2eba4aeaa7d25ae0f0
karamanev <karamanev@gmail.com>
2018-08-25 23:29:46 +0300

    Commit for Heroku

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ const path = require('path');
 
 const app = express();
 
+// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/dabe-dapp'));
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/dabe-dapp/index.html'));
+    
+res.sendFile(path.join(__dirname+'/dist/dabe-dapp/index.html'));
 });
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
