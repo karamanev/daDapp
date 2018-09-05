3fa8d900f5e8508134e6f7407f8762f571b950ba
karamanev <karamanev@gmail.com>
2018-09-05 15:25:27 +0300

    Commit for Heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+      apiKey: "AIzaSyBminGPu-zeb7j5z2aZPVlR20ZMUqkWV1s",
+      authDomain: "testproj-2089a.firebaseapp.com",
+      databaseURL: "https://testproj-2089a.firebaseio.com",
+      storageBucket: "testproj-2089a.appspot.com",
+      projectId: "testproj-2089a",
+  }
 };
